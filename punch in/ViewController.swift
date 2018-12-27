2d5b01d6649d0640bd31fb604b122d64c340ddf0
Justin <jaugu@allstate.com>
2018-12-27 13:44:00 +0530

    Initial Commit

---
+++ punch in/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  punch in
+//
+//  Created by localadmin on 27/12/18.
+//  Copyright © 2018 justin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
